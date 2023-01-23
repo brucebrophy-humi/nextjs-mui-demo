9c409c32fd53d6e96661b422598359033bb9c2fa
Bruce Brophy <bruce@brucebrophy.com>
2023-01-23 08:29:28 -0500

    Add Grid Components

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { CssBaseline } from "@mui/material";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <CssBaseline />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
